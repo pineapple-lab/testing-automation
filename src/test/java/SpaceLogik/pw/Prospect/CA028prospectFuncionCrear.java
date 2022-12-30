970223236932a5c38da253d6c6e72688028f6fea
fubilla <francisco@pineapple-lab.com>
2022-12-30 14:13:03 -0300

    fix caso ca028

--- src/test/java/SpaceLogik/pw/Prospect/CA028prospectFuncionCrear.java
+++ src/test/java/SpaceLogik/pw/Prospect/CA028prospectFuncionCrear.java
@@ -11,15 +11,20 @@ public class CA028prospectFuncionCrear extends robotBaseSpaceLogik {
 
     @Test
     public void CA0281validacionCreacionProspect(){
+        System.out.println("Iniciando caso de prueba...");
         nameCompanyProspect ="pruebAutomatizada10-validacionCreacionProspect";
         assertions = "text=pruebAutomatizada10-validacionCreacionProspect";
-        login();
         iniciarVariablesProspectFeliz();
-        crearProspect();
-        sqlGuardarCasoSiFallaCrearProspect();
-        page.focus(".prospect-list-landlord-table");
-        sqlGuardarCasoSiFallaCrearProspect();
-        Assertions.assertTrue(page.isVisible(assertions));
+        System.out.println("El caso CA0281 se  va e ejecutar "+ejecutar+" veces");
+        for(contador=1;contador<=ejecutar;contador++) {
+            login();
+            crearProspect();
+            sqlGuardarCasoSiFallaCrearProspect();
+            page.focus(".prospect-list-landlord-table");
+            sqlGuardarCasoSiFallaCrearProspect();
+            Assertions.assertTrue(page.isVisible(assertions));
+            System.out.println("El caso CA0281 se ejecuto "+contador+" veces");
+        }
     }
 
 }

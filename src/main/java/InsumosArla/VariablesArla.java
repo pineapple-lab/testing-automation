1f566189a863ff32ce0df0561c3e49b690555090
Francisco Ubilla Croce <francisco@pineapple-lab.com>
2025-05-06 15:16:37 -0300

    actualice user admin

--- src/main/java/InsumosArla/VariablesArla.java
+++ src/main/java/InsumosArla/VariablesArla.java
@@ -15,7 +15,7 @@ public class VariablesArla extends PathUtilsArla{
     public static int setSizeHeight = 980;
     //Variables utiles
     public int executeCounter;
-    public static volatile String username = "admin-b";
+    public static volatile String username = "admin";
     public String password = "123123aA.";
     public int counter = 0;
     public String userRole;

37f6b8186a330259fe26fc097b69ea4fdd830e00
Franciscoubillac <francisco@pineapple-lab.com>
2026-03-16 18:46:05 -0300

    elimine arroba de array de caracteres especiales

--- src/main/java/Configurations/SpecialCaracters.java
+++ src/main/java/Configurations/SpecialCaracters.java
@@ -2,7 +2,7 @@ package Configurations;
 
 public class SpecialCaracters {
     public static String [] specialCaracters = {
-            "!", "@", "#", "$", "%", "^", "&", "*", "(", ")",
+            "!", "#", "$", "%", "^", "&", "*", "(", ")",
             "-", "_", "=", "+", "[", "]", "{", "}", ";", ":",
             "'", "\"", ",", ".", "<", ">", "/", "?", "\\", "|", "~", "`"
     };
